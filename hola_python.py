bb3fc1686f20950cc3c005108f124275516cd03c
misael2910 <misaeleuan2910@gmail.com>
2019-09-24 07:50:48 -0500

    primer commit

---
+++ hola_python.py
@@ -0,0 +1,40 @@
+"""
+Ejercicio de python 2
+Nombre:
+Materia:
+"""
+
+millon = list(range(1,101))
+print(millon)
+
+print(min(millon))
+print(max(millon))
+print(sum(millon))
+
+
+"""
+Fin del ejercicio
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
